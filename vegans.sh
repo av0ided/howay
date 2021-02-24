23a1cddfb41d4c15bcdd086d051645aa2ea9e24e
don <donovandall@icloud.com>
2021-02-24 15:32:24 +0000

    test

--- vegans.sh
+++ vegans.sh
@@ -1,4 +1,4 @@
 
-for (( i = 0; i < 2; i++ )); do
+for (( i = 0; i < 20; i++ )); do
     echo bla >> runs && git add --all && git commit -m "test" && git push origin master && git push origin2 master && git push origin3 master && git push origin4 master && git push origin5 master
 done
